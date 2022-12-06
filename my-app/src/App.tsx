91e03ae70a2c69ffb3965995e189bc6852fe5ff0
hayden <hayden.ismet@amigotechnology.com>
2022-12-06 13:10:28 +0000

    windows build

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import logo from "./logo.svg";
 import "98.css";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        DO IT
-        <button>Hello windows 98</button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="outer-container">
+        <div className="inner-container">
+          <div className="window">
+            <div className="title-bar">
+              <div className="title-bar-text">Portfolio Installation</div>
+              <div className="title-bar-controls">
+                <button aria-label="Close"></button>
+              </div>
+            </div>
+            <div className="window-body">
+              <p>There's so much room for activities!</p>
+              <button>Click me</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
