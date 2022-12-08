9d538c22745366258726b012d40ce7335a4485d6
hayden <hayden.ismet@amigotechnology.com>
2022-12-08 20:06:22 +0000

    close container

---
+++ my-app/src/components/ClosePortfolio.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ClosePortfolio() {
+  return (
+    <>
+      <div className="close-container">You can't leave. Ever.</div>
+    </>
+  );
+}
+
+export default ClosePortfolio;
