a96ccfa3315df8723571134a9f3ed23a9a3b7ed5
hayden <hayden.ismet@amigotechnology.com>
2022-12-09 11:58:33 +0000

    more components, style tweaks and beginning typescript funcs

---
+++ my-app/src/components/About.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <div className="about">
+        <ul className="tree-view">
+          <li>
+            <h3>About</h3>
+          </li>
+          <li>What is web development?</li>
+          <li>
+            CSS
+            <ul>
+              <li>Selectors</li>
+              <li>Specificity</li>
+              <li>Properties</li>
+              <li>Selectors</li>
+              <li>Specificity</li>
+              <li>Properties</li>
+              <li>Selectors</li>
+              <li>Specificity</li>
+              <li>Properties</li>
+              <li>Selectors</li>
+              <li>Specificity</li>
+              <li>Selectors</li>
+              <li>Specificity</li>
+              <li>Properties</li>
+              <li>Selectors</li>
+              <li>Specificity</li>
+            </ul>
+          </li>
+          <li>
+            <details open>
+              <summary>JavaScript</summary>
+              <ul>
+                <li>Avoid at all costs</li>
+                <li>
+                  <details>
+                    <summary>Unless</summary>
+                    <ul>
+                      <li>Avoid</li>
+                      <li>
+                        <details>
+                          <summary>At</summary>
+                          <ul>
+                            <li>Avoid</li>
+                            <li>At</li>
+                            <li>All</li>
+                            <li>Cost</li>
+                          </ul>
+                        </details>
+                      </li>
+                      <li>All</li>
+                      <li>Cost</li>
+                    </ul>
+                  </details>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>HTML</li>
+          <li>Special Thanks</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default About;
--- my-app/src/components/WindowRight.tsx
+++ my-app/src/components/WindowRight.tsx
@@ -1,107 +1,10 @@
 import React from "react";
-
+import About from "./About";
 function WindowRight() {
   return (
     <>
       <div className="right-inner-content">
-        <ul className="tree-view">
-          <li>Table of Contents</li>
-          <li>What is web development?</li>
-          <li>
-            CSS
-            <ul>
-              <li>Selectors</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Selectors</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Selectors</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Selectors</li>
-              <li>Specificity</li>
-              <li>Selectors</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Selectors</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Selectors</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Selectors</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Selectors</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Selectors</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Selectors</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-              <li>Specificity</li>
-              <li>Properties</li>
-            </ul>
-          </li>
-          <li>
-            <details open>
-              <summary>JavaScript</summary>
-              <ul>
-                <li>Avoid at all costs</li>
-                <li>
-                  <details>
-                    <summary>Unless</summary>
-                    <ul>
-                      <li>Avoid</li>
-                      <li>
-                        <details>
-                          <summary>At</summary>
-                          <ul>
-                            <li>Avoid</li>
-                            <li>At</li>
-                            <li>All</li>
-                            <li>Cost</li>
-                          </ul>
-                        </details>
-                      </li>
-                      <li>All</li>
-                      <li>Cost</li>
-                    </ul>
-                  </details>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>HTML</li>
-          <li>Special Thanks</li>
-        </ul>
+        <About />
       </div>
     </>
   );
