56ea0cc39f392b59be8154fbf6f96105055998f4
hayden <hayden.ismet@amigotechnology.com>
2022-12-09 12:53:29 +0000

    footer dynamic

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,13 +3,6 @@ import Window from "./components/Window";
 import "98.css";
 import "./styles/App.css";
 
-function getTime(date: Date): void {
-  let hours: number = date.getHours();
-  console.log(typeof hours, "hour of day ", hours);
-}
-
-getTime(new Date());
-
 function App() {
   return (
     <>
--- my-app/src/components/WindowFooter.tsx
+++ my-app/src/components/WindowFooter.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
+function getTime(date: Date): string {
+  let hours: number = date.getHours();
+  let minutes: number = date.getMinutes();
+  return `${hours}:${minutes}`;
+}
+
 function WindowFooter() {
   return (
     <>
       <div className="status-bar">
-        <p className="status-bar-field">9 object(s)</p>
-        <p className="status-bar-field">10:32AM</p>
+        <p className="status-bar-field">{window.clientInformation.userAgent}</p>
+        <p className="status-bar-field">{getTime(new Date())}</p>
       </div>
     </>
   );
