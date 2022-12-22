4614812fedca305e65403cc12e63a69028470e09
hayden <hayden.ismet@amigotechnology.com>
2022-12-22 15:56:23 +0000

    album image and css tweaks

--- my-app/src/components/portfolio_components/Listening.tsx
+++ my-app/src/components/portfolio_components/Listening.tsx
@@ -3,7 +3,7 @@ import querystring from "querystring";
 import { Buffer } from "buffer";
 import spot_98 from "../../assets/spot_98.png";
 
-//TYPESCRIPT MISSING AFTER
+//Typescript may need a complete restructure here - check through already typescripted bits and any missing typescript bits. See if can be refactored once learnt more or gotten review. Includes things such as interfaces.
 
 function Listening() {
   const [songInfo, setSongInfo] = useState<{
@@ -18,19 +18,28 @@ function Listening() {
   }>({
     item: null,
   });
-  //const [songInfo, setSongInfo] = useState:<songObject | null>(null); // figure out way to set this response so it works. Whole thing may need reengineering and typescript adding
 
   const ENDPOINT: string = "https://accounts.spotify.com/api/token";
+
   const NOW_PLAYING: string =
     "https://api.spotify.com/v1/me/player/currently-playing";
-  //or https://api.spotify.com/v1/me/top/artists || https://api.spotify.com/v1/me/player/recently-played || https://api.spotify.com/v1/me/player/currently-playing - updated with scope for api
 
-  //typescript it with interface....
+  const RECENTLY_PLAYED: string =
+    "https://api.spotify.com/v1/me/player/recently-played";
+
+  const TOP_ARTISTS: string = "https://api.spotify.com/v1/me/top/artists";
+
   const CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
   const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
   const REFRESH_TOKEN = process.env.REACT_APP_SPOTIFY_REFRESH_TOKEN;
 
-  //getNowPlayingItem awaits getNowPlaying() which in itself awaits awaitToken(). Nesting.
+  useEffect(() => {
+    //getNowPlayingItem awaits getNowPlaying() which in itself awaits awaitToken(). Nesting.
+    //getNowPlayingItem(), is called, passing down the three required params from your env setting. These will then flow down into the subsequent funcs as its parent func.
+    getNowPlayingItem(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN);
+  });
+  console.log("songInfo", songInfo);
+
   async function getNowPlayingItem(
     client_id: string | undefined,
     client_secret: string | undefined,
@@ -51,11 +60,6 @@ function Listening() {
     }
   }
 
-  useEffect(() => {
-    //getNowPlayingItem(), is called, passing down the three required params from your env setting. These will then flow down into the subsequent funcs as its parent func.
-    getNowPlayingItem(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN);
-  });
-  console.log("songInfo", songInfo);
   //getNowPlaying func passes await awaitToken(), waits for token to return, then passes that into a fetch for NOW_PLAYING url.
   async function getNowPlaying(
     client_id: string | undefined,
@@ -101,18 +105,30 @@ function Listening() {
     <>
       <div className="listening">
         <ul className="tree-view">
-          <li>
+          <li className="listening-header">
+            <img src={spot_98} alt="spotify-logo" className="spotify-logo-98" />
             <h3>Listening</h3>
           </li>
           {songInfo && songInfo.item && songInfo.item.name
             ? songInfo.item.name
             : "Not loaded yet"}
-          <li>
-            <img src={spot_98} alt="spotify-logo" className="spotify-logo-98" />
-          </li>
-          <li>
-            <h3>ss</h3>
-          </li>
+
+          {songInfo &&
+          songInfo.item &&
+          songInfo.item.album &&
+          songInfo.item.album.images &&
+          songInfo.item.album.images[2] &&
+          songInfo.item.album.images[2].url ? (
+            <li>
+              <img
+                src={songInfo.item.album.images[2].url}
+                alt="current-track-cover"
+                className="current-track-cover"
+              />
+            </li>
+          ) : (
+            "Loading album image"
+          )}
         </ul>
       </div>
     </>
